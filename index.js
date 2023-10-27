2dcb914b228ba1663fe62753c40b1674f5f70b50
PDXMelon <pdxrabbit.coding@proton.me>
2023-10-27 11:53:02 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs')
+
+// Create a server object
+http.createServer(function (req, res) {
+
+	// http header
+	res.writeHead(200, {'Content-Type': 'text/html'}); 
+
+	const url = req.url;
+
+	if(url === '/webstorex') {
+		let html = fs.readFileSync('main.html')
+		res.end(html); 
+	}
+}).listen(3000, function() {
+	// The server object listens on port 3000
+	console.log("server start at port 3000");
+});
